116871e6ea6a1b960324e07f6f61c447b5ec6a83
Egor Komarov <komarovev91@gmail.com>
2025-12-24 12:57:12 +0100

    refactor: update footer include method and Cloudflare Turnstile integration

--- public/js/gate.js
+++ public/js/gate.js
@@ -42,7 +42,7 @@ window.onloadTurnstileCallback = function () {
     callback: onTurnstileSuccess,
     'error-callback': onTurnstileError,
     'expired-callback': onTurnstileError,
-    theme: 'light',
+    size: 'compact',
   });
 };
 
